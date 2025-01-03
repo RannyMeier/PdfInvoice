95089587e6f758ad9c46b59f45bb71a684d73091
Ranny Meier <Ranny@RespMech.com>
2025-01-03 13:38:44 -0600

    Add project files.

---
+++ PdfInvoice/OrdService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace PdfInvoice
+{
+
+    public static class OrdService
+    {
+
+        public static async Task<TrxnDocV> GetDocVByDN(HttpClient http, string docNum)
+        {
+            var response = await http.GetAsync($"https://wcabr.app/API/GetDocV/ByDN/{docNum}");
+            if (response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    return default;
+                }
+                return await response.Content.ReadFromJsonAsync<TrxnDocV>();
+            }
+            // Refine exception handling
+            var message = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Http Status code: {response.StatusCode} Message: {message}");
+        }
+
+
+    }
+
+}
